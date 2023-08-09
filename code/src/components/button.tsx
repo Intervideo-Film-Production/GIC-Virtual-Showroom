592f0a3e64290debdadd561b1059ad751d85c743
sammytran90 <32162425+sammytran90@users.noreply.github.com>
2023-08-09 21:04:00 +0100

    initial structure

---
+++ code/src/components/button.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { styled } from "styled-components";
+
+interface IButton {
+
+}
+
+const Button = styled.button<IButton>(({ }) => ({
+
+}));
+
+export default Button;
---
+++ code/src/containers/scene.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Scene: React.FC = () => {
+  return (
+    <div>
+      scene
+    </div>
+  )
+}
+
+export default Scene;
