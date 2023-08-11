64a06852827d57d04fae59180fc5829357c0ffde
sammytran90 <32162425+sammytran90@users.noreply.github.com>
2023-08-11 11:50:06 +0100

    basic types

---
+++ code/src/core/@types/index.d.ts
@@ -0,0 +1,59 @@
+// TODO need this?
+export interface IEntity {
+  id: string;
+}
+
+export interface IExperience {
+  title: string;
+}
+
+export interface IPosition {
+  positionX: number;
+  positionY: number;
+}
+
+export interface I3DPosition extends IPosition {
+  positionZ: number;
+}
+
+export interface IViewpoint {
+  image: "asset" // TODO
+  room: IRoom;
+  enableViewpoints: IViewpoint[];
+  // enableHHotspots: IHotspot[];
+}
+
+export interface IViewpointPosition extends IPosition {
+  viewpoint: IViewpoint;
+}
+
+export interface IHotspot {
+  viewpoint: IViewpoint;
+  content: daksdajdjsa;
+}
+
+export interface IHotspotPosition extends I3DPosition { 
+  hotspot: IHotspot;
+  rotation: 3DRotation;
+  size: 3DPlaneSize;
+}
+
+export interface IPortal extends I3DPosition { }
+
+export interface IRoom {
+  title: string;
+  firstViewpoint: IViewpoint;
+}
+
+export interface IMapRoom extends IPosition {
+  room: IRoom;
+}
+
+export interface IMap {
+  image: "asset" // TODO need real type 
+  rooms: IMapRoom[];
+}
+
+export interface ITour {
+  title: string;
+}
---
+++ code/src/crud/crud.ts
@@ -0,0 +1,10 @@
+
+
+export const getExperiences = () => { }
+export const getExperience = (id: string) => { }
+
+export const getRooms = () => { }
+export const getRoom = (id: string) => { }
+
+export const getTours = () => {}
+export const getTour = (id: string) => {}
